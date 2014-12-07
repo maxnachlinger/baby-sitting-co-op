8d9ccbb9a69222368776ac8e9b89d4be1fae77da
maxnachlinger <max.nachlinger@gmail.com>
2014-12-06 20:25:18 -0800

    Added directories for client and server, and that's about it.

---
+++ server/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var net = require('net');
+var multilevel = require('multilevel');
+var level = require('level');
+var sublevel = require('level-sublevel');
+var VError = require('verror');
+var config = require('../config');
+
+module.exports = function () {
+	var db = sublevel(level(config.dbPath, {
+		createIfMissing: true,
+		errorIfExists: false,
+		valueEncoding: 'json'
+	}));
+	var transactionsLevel = db.sublevel(config.subLevels.transactions);
+	var statsLevel = db.sublevel(config.subLevels.stats);
+
+	net.createServer(function (con) {
+		con.pipe(multilevel.server(db)).pipe(con);
+	}).listen(config.levelPort, function (err) {
+		if (err) throw new VError(err);
+
+		// setup all the things here
+	});
+};
